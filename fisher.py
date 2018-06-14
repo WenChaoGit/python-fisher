b91904ae5579d0b230830d92e47d52a68aefe597
WenChaoGit <wenchaoyang@outlook.com>
2018-06-14 18:17:10 +0800

    flask response

--- fisher.py
+++ fisher.py
@@ -1,12 +1,17 @@
-
-from flask import Flask
+from flask import Flask, make_response
 from config import DEBUG
 
 __author__ = '文朝'
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def hello():
-   return 'hello,world'
+    headers = {
+        'content-type': 'text/plain'
+    }
+    return '<html/>', 301, headers
 
-app.run(debug=DEBUG)
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG)
